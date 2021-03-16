1fbbff68f3d41e9540064fa875e2819fc7493706
Mad Gray Hatter <gbiagomba@users.noreply.github.com>
2021-03-16 12:07:16 -0400

    Added masscan; added ipv6 scanning; fixed nmap err

--- little-raven.sh
+++ little-raven.sh
@@ -29,13 +29,14 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Declaring variables
-TodaysDAY=$(date +%m-%d)
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 TodaysYEAR=$(date +%Y)
-wrkpth=$(pwd)
+wrkpth=$PWD
 targets=$1
+prj_name=$2
 
 # Setting Envrionment
-mkdir -p $wrkpth/Nmap/
+mkdir -p $wrkpth/tools/nmap $wrkpth/tools/masscan
 
 # Requesting target file name or checking the target file exists & requesting the project name
 if [ -z $targets ]; then
@@ -47,11 +48,11 @@ elif [ ! -e $targets ]; then
     exit
 fi
 
-echo "What is the name of the project or SAS?"
-read prj_name
-echo
-
 if [ -z $prj_name ]; then
+    echo "What is the name of the project or SAS?"
+    read prj_name
+    echo
+elif [ -z $prj_name ]; then
     prj_name=`echo $RANDOM`
 fi
 
@@ -61,13 +62,13 @@ fi
 echo "--------------------------------------------------"
 echo "Nmap Pingsweep - ICMP echo, netmask, timestamp & TCP SYN, and UDP"
 echo "--------------------------------------------------"
-nmap -T5 --host-timeout 30m --randomize-hosts -PA "21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PE -PM -PP -PS "21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PU "42,53,67-68,88,111,123,135,137,138,161,500,3389,5355" -PY "22,80,179,5060" -R --reason --resolve-all -sn -iL $targets -oA $wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR
-nmap -PA"21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PE -PS"21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PU"42,53,67-68,88,111,123,135,137,138,161,500,3389,5355" -PY"22,80,179,5060" -T5 -R --reason --resolve-all -sn -6 -iL $targets -oA $wrkpth/Nmap/$prj_name-nmap_pingsweepv6-$TodaysDAY-$TodaysYEAR
-if [ -s $wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR.gnmap ] && [ -r $wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR.gnmap ]; then
-    cat $wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR.gnmap | grep Up | cut -d ' ' -f 2 | sort | uniq >> $wrkpth/Nmap/$prj_name-livehosts-$TodaysDAY-$TodaysYEAR
-    cat $wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR.gnmap | grep -E "(\.gov|\.us|\.net|\.com|\.edu|\.org|\.biz|\.io|\.info)" | cut -d "(" -f 2 | cut -d ")" -f 1 | sort | uniq >> $wrkpth/Nmap/$prj_name-livehosts-$TodaysDAY-$TodaysYEAR
-elif [ ! -s $wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR.gnmap ]; then
-    echo "$wrkpth/Nmap/$prj_name-nmap_pingsweep-$TodaysDAY-$TodaysYEAR.gnmap does not exist"
+nmap -T5 --host-timeout 30m --randomize-hosts -PA "21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PE -PM -PP -PS "21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PU "42,53,67-68,88,111,123,135,137,138,161,500,3389,5355" -PY "22,80,179,5060" -R --reason --resolve-all -sn -iL $targets -oA $wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time
+nmap -T5 --host-timeout 30m --randomize-hosts -PA"21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PE -PS"21-23,25,53,80,88,110,111,135,139,443,445,3389,8080" -PU"42,53,67-68,88,111,123,135,137,138,161,500,3389,5355" -PY"22,80,179,5060" -T5 -R --reason --resolve-all -sn -6 -iL $targets -oA $wrkpth/tools/nmap/$prj_name-nmap_pingsweepv6-$current_time
+if [ -s $wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time.gnmap ] && [ -r $wrkpth/tools/nmap/$prj_name-nmap_pingsweepv6-$current_time.gnmap ]; then
+    cat $wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time.gnmap $wrkpth/tools/nmap/$prj_name-nmap_pingsweepv6-$current_time | grep Up | cut -d ' ' -f 2 | sort -u >> $wrkpth/tools/nmap/$prj_name-livehosts-$current_time.list
+    cat $wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time.gnmap $wrkpth/tools/nmap/$prj_name-nmap_pingsweepv6-$current_time | cut -d "(" -f 2 | cut -d ")" -f 1 | sort -u >> $wrkpth/tools/nmap/$prj_name-livehosts-$current_time.list
+elif [ ! -s $wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time.gnmap  && [ ! -s $wrkpth/tools/nmap/$prj_name-nmap_pingsweepv6-$current_time ]; then
+    echo "$wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time.gnmap does not exist"
     echo "Check stdout (terminal output) for any errors in nmap & check internet connection"
     exit
 fi
@@ -80,16 +81,23 @@ echo "--------------------------------------------------"
 echo "Performing portknocking scan using Nmap"
 echo "--------------------------------------------------"
 echo "Full TCP SYN & UDP scan on live targets"
-nmap -A -Pn -R --reason --resolve-all -sSUV --randomize-hosts -T4 --open -F -iL $wrkpth/Nmap/$prj_name-livehosts-$TodaysDAY-$TodaysYEAR -oA $wrkpth/Nmap/$prj_name-nmap_portknock-$TodaysDAY-$TodaysYEAR
-if [ -r $wrkpth/Nmap/$prj_name-nmap_portknock-$TodaysDAY-$TodaysYEAR ]; then
-    for i in domain http imap isakmp microsoft-ds ms-wbt-server netbios-ssn smtp snmp ssh ssl telnet; do cat $wrkpth/Nmap/$prj_name-nmap_portknock-$TodaysDAY-$TodaysYEAR | grep $i | grep open | cut -d ' ' -f 2 > $wrkpth/Nmap/$prj_name-`echo $i | tr '[:lower:]' '[:upper:]'`-$TodaysDAY-$TodaysYEAR; done
-    for i in `ls | grep xml`; do python3 /opt/nmap-converter/nmap-converter.py -o "$wrkpth/Nmap/$prj_name-nmap_portknock-$TodaysDAY-$TodaysYEAR.xlsx" "$i"; python3 /opt/nmaptocsv/nmaptocsv.py -x "$i" -S -d "," -n -o $wrkpth/Nmap/$i-$TodaysDAY-$TodaysYEAR.csv; done
-    batea -A $wrkpth/Nmap/$prj_name-*.xml > $wrkpth/Nmap/$prj_name-batea-$TodaysDAY-$TodaysYEAR.json
-elif [ ! -s $wrkpth/Nmap/$prj_name-nmap_portknock-$TodaysDAY-$TodaysYEAR ]; then
-    echo "$wrkpth/Nmap/$prj_name-nmap_portknock-$TodaysDAY-$TodaysYEAR does not exist"
+nmap -A -Pn -R --reason --resolve-all -sSUV --randomize-hosts -T4 --open -F --scripts "vulners,vuln,auth,brute,targets-xml" --script-args=newtargets,iX=$wrkpth/tools/nmap/$prj_name-nmap_pingsweep-$current_time.xml -oA $wrkpth/tools/nmap/$prj_name-nmap_fs-$current_time
+nmap -6 -A -Pn -R --reason --resolve-all -sSUV --randomize-hosts -T4 --open -F --scripts "vulners,vuln,auth,brute,targets-xml" --script-args=newtargets,iX=$wrkpth/tools/nmap/$prj_name-nmap_pingsweepv6-$current_time.xml -oA $wrkpth/tools/nmap/$prj_name-nmap_fs6-$current_time
+if [ -r $wrkpth/tools/nmap/$prj_name-nmap_fs-$current_time.gnmap ] || [ -r $wrkpth/tools/nmap/$prj_name-nmap_fs6-$current_time.gnmap ]; then
+    for i in domain http imap isakmp microsoft-ds ms-wbt-server netbios-ssn smtp snmp ssh ssl telnet; do cat $wrkpth/tools/nmap/$prj_name-nmap_fs-$current_time.gnmap $wrkpth/tools/nmap/$prj_name-nmap_fs6-$current_time.gnmap | grep $i | grep open | cut -d ' ' -f 2 > $wrkpth/tools/nmap/$prj_name-`echo $i | tr '[:lower:]' '[:upper:]'`$current_time.list; done
+    for i in `ls | grep xml`; do python3 /opt/nmap-converter/nmap-converter.py -o "$wrkpth/tools/nmap/$prj_name-nmap_fs$current_time.xlsx" "$i"; python3 /opt/nmaptocsv/nmaptocsv.py -x "$i" -S -d "," -n -o $wrkpth/tools/nmap/$i$current_time.csv; done
+    batea -A $wrkpth/tools/nmap/$prj_name-*.xml > $wrkpth/tools/nmap/$prj_name-batea$current_time.json
+elif [ ! -s $wrkpth/tools/nmap/$prj_name-nmap_fs-$current_time.gnmap ] && [ ! -s $wrkpth/tools/nmap/$prj_name-nmap_fs6-$current_time.gnmap ]; then
+    echo "$wrkpth/tools/nmap/$prj_name-nmap_fs-$current_time.gnmap does not exist"
     echo "Check stdout (terminal output) for any errors in nmap & check internet connection"
     exit
 echo
+
+echo "--------------------------------------------------"
+echo "Performing portknocking scan using Nmap"
+echo "--------------------------------------------------"
+echo "Full TCP SYN & UDP scan on live targets"
+masscan --rate 1000 --banners --open-only --retries 3 -p 0-65535 -iL $wrkpth/tools/nmap/$prj_name-livehosts-$current_time.list -oA $wrkpth/tools/masscan/$prj_name-masscan-$current_time.list
 
 # ------------------------------------------------------
 # CLEANING HOUSE
@@ -106,6 +114,6 @@ find $wrkpth/Nmap -type d,f -empty | xargs rm -rf
 zip -ru9 $wrkpth/$prj_name-$TodaysYEAR.zip $wrkpth/Nmap
 
 # Uninitializing variables
-for var in TodaysDAY TodaysYEAR wrkpth wrktmp targets ; do unset $var; done
+for var in TodaysYEAR wrkpth wrktmp targets ; do unset $var; done
 unset var
 exit
